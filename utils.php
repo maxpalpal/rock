0b9e70f45efbfceb2786f4ac46717dad42a99e91
maxpalpal <palestinianpy@gmail.com>
2017-03-09 13:43:12 +0200

    Update utils.php

--- utils.php
+++ utils.php
@@ -1,12 +1,11 @@
 <?php
-$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-$server = $url["host"];
-$username = $url["user"];
-$password = $url["pass"];
-$db = substr($url["path"], 1);
-$conn = new mysqli($server, $username, $password, $db);
-echo $conn;
 
-echo "maaaaaaaaaaa";
+$con = pg_connect("host=ec2-54-204-32-145.compute-1.amazonaws.com port=5432 dbname=dc0lv0spd5muji user=wulcydgyttdqij password=b4fc873362e82f6a785efaaf9439b5a6bb214ce33a4a71444058f4ce4f1ee92d sslmode=require options='--client_encoding=UTF8'") or die('Could not connect: ' . pg_last_error()); 
+ 
+$query = "INSERT INTO man(msg)VALUES ('
+dfdfdfdfdf')"; 
+pg_query($con, $query) or die("Cannot execute query: $query\n");
+
+pg_close($con); 
 ?>
-3333333333
+
