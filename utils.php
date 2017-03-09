751749cc27ec6ef2dc0c4d910388b1e3e26ebf6b
maxpalpal <palestinianpy@gmail.com>
2017-03-09 13:46:55 +0200

    Update utils.php

--- utils.php
+++ utils.php
@@ -1,11 +1,1 @@
-<?php
-
-$con = pg_connect("host=ec2-54-204-32-145.compute-1.amazonaws.com port=5432 dbname=dc0lv0spd5muji user=wulcydgyttdqij password=b4fc873362e82f6a785efaaf9439b5a6bb214ce33a4a71444058f4ce4f1ee92d sslmode=require options='--client_encoding=UTF8'") or die('Could not connect: ' . pg_last_error()); 
- 
-$query = "INSERT INTO man(msg)VALUES ('
-dfdfdfdfdf')"; 
-pg_query($con, $query) or die("Cannot execute query: $query\n");
-
-pg_close($con); 
-?>
 
